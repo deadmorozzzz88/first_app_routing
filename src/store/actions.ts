513c69e2f6e8a841d4a69bce34fc9a203b066ab0
Nikita Popov <n.popov88@mail.ru>
2022-02-06 14:51:30 +0300

    redux finish path

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,12 +1,35 @@
+import { UserType } from "./types";
 import { PostsDataType } from "../assets/data";
 /* eslint-disable no-unused-vars */
 export enum PostsActionType {
   fetchPostsData = "FETCH_POST_DATA",
 }
 
+export enum UserActionsType {
+  userRegistration = "USER_REGISTRATION",
+  userLogout = "USER_LOGOUT",
+}
+
 export const fetchPostsDataAction = (data: PostsDataType) => {
   return {
     type: PostsActionType.fetchPostsData,
     payload: data,
+  };
+};
+
+export const userRegistrationAction = (user: UserType) => {
+  const editUser = {
+    lastName: user.lastName.toLocaleLowerCase(),
+    firstName: user.firstName.toLocaleLowerCase(),
+  };
+  return {
+    type: UserActionsType.userRegistration,
+    payload: editUser,
+  };
+};
+
+export const userLogoutAction = () => {
+  return {
+    type: UserActionsType.userLogout,
   };
 };
